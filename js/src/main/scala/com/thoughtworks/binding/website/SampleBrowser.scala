cc0223b317c34563548ff686770582feade48c6a
Yang Bo <atryyang@thoughtworks.com>
2016-02-06 00:17:05 +0800

    Switch samples via location.hash

--- js/src/main/scala/com/thoughtworks/binding/website/SampleBrowser.scala
+++ js/src/main/scala/com/thoughtworks/binding/website/SampleBrowser.scala
@@ -1,28 +1,36 @@
 package com.thoughtworks.binding.website
 
-import com.thoughtworks.binding.Binding.{Var, Vars}
+import com.thoughtworks.binding.Binding.{ Var, Vars }
 import com.thoughtworks.binding.dom
+import org.scalajs.dom.location
+import org.scalajs.dom.window
 import org.scalajs.dom.document
-import org.scalajs.dom.raw.Event
+import org.scalajs.dom.raw.{ Event, HashChangeEvent }
+import scala.util._
 
 import scala.scalajs.js.annotation.JSExport
 
 /**
-  * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
-  */
+ * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
+ */
 @JSExport
 object SampleBrowser {
 
-
   val Samples = Seq(IntSample, TableSample)
+
+  private def hashIndex = Try(location.hash.substring(1).toInt).getOrElse(0)
 
   @dom
   def render = {
-    val currentSampleIndex = Var(0)
+    val currentSampleIndex = Var(hashIndex)
 
+    window.onhashchange = { event: HashChangeEvent =>
+      currentSampleIndex := hashIndex
+    }
+    location.hash = s"#${currentSampleIndex.each.toString}"
 
     <section>
-      <button onclick={event: Event =>
+      <button onclick={ event: Event =>
         currentSampleIndex := {
           if (currentSampleIndex.get + 1 == Samples.length) {
             0
@@ -37,13 +45,13 @@ object SampleBrowser {
       {
         val sample = Samples(currentSampleIndex.each)
         <section>
-          {sample.render.each}
+          { sample.render.each }
           <hr/>
           <fieldset>
             <legend>
-              {sample.fileName}
+              { sample.fileName }
             </legend>
-            <pre><code>{sample.content}</code></pre>
+            <pre><code>{ sample.content }</code></pre>
           </fieldset>
         </section>
       }
