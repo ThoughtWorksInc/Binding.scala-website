3e278c3bf72ff65816f6996fd277299ff930656e
Yang Bo <atryyang@thoughtworks.com>
2016-06-18 07:58:28 +0800

    Rename CommandLint to Repl

--- js/src/main/scala/com/thoughtworks/binding/website/Repl.scala
+++ js/src/main/scala/com/thoughtworks/binding/website/Repl.scala
@@ -1,10 +1,14 @@
 package com.thoughtworks.binding.website
 
-import com.thoughtworks.binding.dom
-import org.scalajs.dom.raw.Event
 import com.thoughtworks.binding.Binding.Vars
+import com.thoughtworks.binding.dom
 
-object CommandList extends Sample {
+import scala.scalajs.js
+
+import org.scalajs.dom.window
+import org.scalajs.dom.raw.Event
+
+object Repl extends Sample {
 
   override def fileName = CurrentSource.fileName
 
@@ -16,6 +20,8 @@ object CommandList extends Sample {
     val input = <input type="text"/>
     val submitHandler = { event: Event =>
       logs.get += input.value
+      logs.get += js.eval(input.value).toString
+      input.value = ""
       event.preventDefault()
     }
     <form onsubmit={ submitHandler }>
