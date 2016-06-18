9dfd8de426f430cccbbafab667a6110fb2434629
Yang Bo <atryyang@thoughtworks.com>
2016-06-19 01:52:51 +0800

    Add a CounterWithTime DEMO

---
+++ js/src/main/scala/com/thoughtworks/binding/website/CounterWithTime.scala
@@ -0,0 +1,29 @@
+package com.thoughtworks.binding.website
+
+import com.thoughtworks.binding.Binding.Var
+import com.thoughtworks.binding.Binding
+import java.util.Date
+import com.thoughtworks.binding.dom
+import org.scalajs.dom.raw.Event
+
+object CounterWithTime extends Sample {
+
+  override def fileName = CurrentSource.fileName
+
+  override def content = CurrentSource.content
+
+  val count = Var(0)
+
+  @dom def status: Binding[String] = {
+    val startTime = new Date
+    "本页面初始化的时间是" + startTime.toString + "。按钮被按过" + count.each.toString + "次。按钮最后一次按下的时间是" + (new Date).toString
+  }
+
+  @dom def render = {
+    <div>
+      { status.each }
+      <button onclick={ event: Event => count := count.get + 1 }>更新状态</button>
+    </div>
+  }
+
+}
