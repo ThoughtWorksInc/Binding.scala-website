6ede1be37699818bf5d05b657cd2a5b77fae6627
Yang Bo <atryyang@thoughtworks.com>
2016-07-01 06:46:04 +0800

    Add comment

--- js/src/main/scala/com/thoughtworks/binding/website/GithubAvatar.scala
+++ js/src/main/scala/com/thoughtworks/binding/website/GithubAvatar.scala
@@ -21,18 +21,18 @@ object GithubAvatar extends Sample {
       <hr/>
       {
         val name = githubUserName.each
-        if (name == "") {
-          <div>Please input your Github user name</div>
-        } else {
-          val githubResult = FutureBinding(Ajax.get(s"https://api.github.com/users/${name}"))
-          githubResult.each match {
-            case None =>
-              <div>Loading the avatar for { name }</div>
-            case Some(Success(response)) =>
-              val json = JSON.parse(response.responseText)
-              <img src={ json.avatar_url.toString }/>
-            case Some(Failure(exception)) =>
-              <div>{ exception.toString }</div>
+        if (name == "") {                                                                     // 如果用户名为空：
+          <div>Please input your Github user name</div>                                       // 显示提示文字；
+        } else {                                                                              // 如果用户名非空：
+          val githubResult = FutureBinding(Ajax.get(s"https://api.github.com/users/${name}")) // 发起 Github API 请求，
+          githubResult.each match {                                                           // 并检查 API 结果。
+            case None =>                                                                      // 如果尚未加载完毕：
+              <div>Loading the avatar for { name }</div>                                      // 显示提示信息；
+            case Some(Success(response)) =>                                                   // 如果成功加载：
+              val json = JSON.parse(response.responseText)                                    // 把回应解析成 JSON；
+              <img src={ json.avatar_url.toString }/>                                         // 并显示头像；
+            case Some(Failure(exception)) =>                                                  // 如果加载时出错，
+              <div>{ exception.toString }</div>                                               // 显示错误信息。
           }
         }
       }
